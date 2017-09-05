508ecc5c2c5d2509c18ca1d77be21dc4468cdd92
Oscar de León <odeleongt@gmail.com>
2017-09-05 11:37:02 -0600

    Add useful exports from magrittr, tibble, dplyr and purrr

---
+++ R/utils.r
@@ -0,0 +1,15 @@
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
+
+#' @importFrom tibble data_frame
+#' @export
+tibble::data_frame
+
+#' @importFrom dplyr mutate
+#' @export
+dplyr::mutate
+
+#' @importFrom purrr map2
+#' @export
+purrr::map2
